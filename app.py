780920646fe2cbcc933bedd7f9cd987dc95834c7
Subhasree Sahu <subhasreesahu02@gmail.com>
2025-10-02 14:34:27 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -149,4 +149,6 @@ def upload_file():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))  # Use the platform-provided port
+    app.run(host="0.0.0.0", port=port)       # Bind to all interfaces
